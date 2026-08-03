1708e9411330a3f3452cb99858b9d2a1fe476eee
focacceriadellelanghe <focacceriadellelanghe@gmail.com>
2026-08-03 17:30:37 +0200

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.FCI_CONFIG = {
-  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby8m07UVjrodTBraNECq_7AjtAsbW7-vGF_AHU1pmKjFUpHZgIwuKT-RYpElpFgc9Yq/exec',
+window.FCI_CONFIG = Object.freeze({
+  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby8m07UVjrodTBraNECq_7AjtAsbW7-vGF_AHU1pmKjFUpHZgIwuKT-RYpElpFgC9Yq/exec',
   API_TOKEN: ''
-};
+});
